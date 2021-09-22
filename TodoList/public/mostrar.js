babdeb2e544e3cd1e53b1b7673c47466bcc02532
Thiago Henrique <90797771+ThiagoHenrique323@users.noreply.github.com>
2021-09-22 15:20:39 -0300

    Delete mostrar.js

--- TodoList/public/mostrar.js
+++
@@ -1,7 +0,0 @@
-function ler(){
-  for(cont = 1; cont <= localStorage.cont; cont++){
-    lista = localStorage.getItem('desc_'+cont);
-    document.getElementById("cads").innerHTML += "Descrição: "+  lista.split(";")[0] + "<br>"+ " Data limite: "+ lista.split(";")[1]+ "<br>"+" Data de criação: "+ lista.split(";")[2]+"<br>"+" Status: Incompleto"+ "<br> <br>";
-   //Bom é provisorio esse Status de incompleto rsrs.
-  }
-}
